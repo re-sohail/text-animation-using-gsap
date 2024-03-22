1edf447e6d02b679609a9365ca272835bd4f28ae
Sohail Khan <business.yasrabtv@gmail.com>
2024-03-22 15:54:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+gsap.from('.first h1',{
+  opacity: 0,
+  y: 40,
+  duration: 0.5,
+  delay: .5,
+  stagger: 0.2,
+})
+
+gsap.from('.second h1',{
+  duration: 1, 
+  delay: 0.5, 
+  opacity: 0, 
+  rotationX: -80, 
+  force3D: true, 
+  transformOrigin: "top center -50", 
+  stagger: 0.1,
+  scrollTrigger: {
+      trigger: ".second",
+      scroller: "body",
+      start: "top 30%",
+      end: "top -20%",
+      scrub: 0.2,
+      markers: true
+  }
+})
+
+gsap.from('.third h1',{
+  y: 40,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.1,
+  scrollTrigger: {
+      trigger: ".third",
+      scroller: "body",
+      start: "top 30%",
+      end: "top -20%",
+      scrub: 0.2,
+      markers: true
+  }
+})
+
+gsap.from('.fourth h1',{
+  y: 40,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.1,
+  scrollTrigger: {
+      trigger: ".fourth",
+      scroller: "body",
+      start: "top 30%",
+      end: "top -20%",
+      scrub: 0.2,
+      markers: true
+  }
+})
+
+gsap.from('.Fifth h1',{
+  y: 40,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.1,
+  scrollTrigger: {
+      trigger: ".Fifth",
+      scroller: "body",
+      start: "top 30%",
+      end: "top -20%",
+      scrub: 0.2,
+      markers: true
+  }
+})
+
+
+
+
